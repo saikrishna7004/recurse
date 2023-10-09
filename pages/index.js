a45be449de881e32c248c8b5736c81ed4b449074
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-10-09 17:57:14 +0530

    Added recurse navraas link

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,7 @@ const HomePage = () => {
 				<p>The Hunt begins in: {countdown}</p>
 				<p>The hunt starts at 7:00 pm today</p>
 			</div>
+			<Link href={startButtonDisabled?'/':"https://recurse-navraas.vercel.app/"} className={`button ${startButtonDisabled ? 'disabled' : ''}`}>Start Hunt</Link>
 		</div>
 	);
 };
