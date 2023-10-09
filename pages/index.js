e68ae6069ed9c2eed9164a5e52e20785169de59f
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-10-09 15:41:38 +0530

    Added timer

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
+const HomePage = () => {
+	// Set your target date and time for the treasure hunt
+	const targetDate = new Date('2023-10-09T18:00:00').getTime();
+	const [countdown, setCountdown] = useState('');
+	const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
 
-export default function Home() {
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const now = new Date().getTime();
+			const timeLeft = targetDate - now;
+
+			if (timeLeft <= 0) {
+				setCountdown('Treasure Hunt has started!');
+				setStartButtonDisabled(false); // Enable the button when the timer reaches zero
+				clearInterval(interval);
+			} else {
+				const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+				const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+				setCountdown(`${hours}h ${minutes}m ${seconds}s`);
+			}
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
-		<div className='container'>
+		<div className='container' style={{height: '90vh', display: 'flex', textAlign: 'center', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
 			<img src="recurse logo white.png" alt="Recurse Logo White" />
-			<h1 className='title'>Recurse Official</h1>
-			<p>
-				Something is comming up!!
-			</p>
+			<h1 className='title'>Online Treasure Hunt</h1>
+			<div className='countdown'>
+				<p>The Hunt begins in: {countdown}</p>
+			</div>
 		</div>
-	)
-}
+	);
+};
+
+export default HomePage;
