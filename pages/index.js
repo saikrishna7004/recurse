cde1e8a41291a12817795a7de52afe2ab68db5f0
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-10-10 01:38:39 +0530

    Added hunt started message

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import Link from 'next/link';
 
 const HomePage = () => {
 	// Set your target date and time for the treasure hunt
-	const targetDate = new Date('2023-10-09T19:00:00').getTime();
-	const [countdown, setCountdown] = useState('');
-	const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
+	// const targetDate = new Date('2023-10-09T19:00:00').getTime();
+	// const [countdown, setCountdown] = useState('');
+	// const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
 
 	return (
 		<div className='container' style={{height: '90vh', display: 'flex', textAlign: 'center', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
 			<img src="recurse logo white.png" alt="Recurse Logo White" />
 			<h1 className='title'>Online Treasure Hunt</h1>
-			<div className='countdown'>
-				<p>The hunt starts at 7:00 pm today</p>
-				<p>Use only <b>Laptop</b>, or you will see something else.</p>
+			<p>The hunt has already started</p>
+			<p>Use only <b>Laptop</b>, or you will see something else.</p>
+			<div style={{margin: '40px 0'}}>
+				<Link className='button' href={"https://recurse-navraas.vercel.app/"}>Go to Hunt Page</Link>
 			</div>
-			<Link className='button' href={"https://recurse-navraas.vercel.app/"}>Main Site</Link>
 		</div>
 	);
 };
