b51360e0ec4f0aab801e496ee396795f9e2591a4
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-10-09 18:55:45 +0530

    Added mobole warning

--- pages/index.js
+++ pages/index.js
@@ -7,34 +7,13 @@ const HomePage = () => {
 	const [countdown, setCountdown] = useState('');
 	const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			const now = new Date().getTime();
-			const timeLeft = targetDate - now;
-
-			if (timeLeft <= 0) {
-				setCountdown('Treasure Hunt has started!');
-				setStartButtonDisabled(false); // Enable the button when the timer reaches zero
-				clearInterval(interval);
-			} else {
-				const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-				const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-				setCountdown(`${hours}h ${minutes}m ${seconds}s`);
-			}
-		}, 1000);
-
-		return () => clearInterval(interval);
-	}, []);
-
 	return (
 		<div className='container' style={{height: '90vh', display: 'flex', textAlign: 'center', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
 			<img src="recurse logo white.png" alt="Recurse Logo White" />
 			<h1 className='title'>Online Treasure Hunt</h1>
 			<div className='countdown'>
-				<p>The Hunt begins in: {countdown}</p>
 				<p>The hunt starts at 7:00 pm today</p>
+				<p>Use only <b>Laptop</b>, or you will see something else.</p>
 			</div>
 			<Link className='button' href={"https://recurse-navraas.vercel.app/"}>Main Site</Link>
 		</div>
